94e5d799dd0928e1fc3d91ab9774a3758b72d085
sejka <karol.sejka@gmail.com>
2018-05-29 01:23:44 +0200

    disables mailkit and support for smtp server

--- Core/EmailSenders/SmtpSender.cs
+++ Core/EmailSenders/SmtpSender.cs
@@ -1,53 +1,53 @@
-﻿using MailKit.Net.Smtp;
-using MimeKit;
-using System;
-using System.Threading.Tasks;
+﻿//using MailKit.Net.Smtp;
+//using MimeKit;
+//using System;
+//using System.Threading.Tasks;
 
-namespace Core.EmailSenders
-{
-    //todo should i test this?
-    public class SmtpSender : IEmailSender
-    {
-        private SmtpSenderOptions _options;
-        private SmtpClient _smtpClient;
+//namespace Core.EmailSenders
+//{
+//    //todo should i test this?
+//    public class SmtpSender : IEmailSender
+//    {
+//        private SmtpSenderOptions _options;
+//        private SmtpClient _smtpClient;
 
-        public SmtpSender(SmtpSenderOptions options)
-        {
-            _options = options;
-            _smtpClient = new SmtpClient();
-        }
+//        public SmtpSender(SmtpSenderOptions options)
+//        {
+//            _options = options;
+//            _smtpClient = new SmtpClient();
+//        }
 
-        public void Connect()
-        {
-            _smtpClient.Connect(_options.Host, _options.Port, MailKit.Security.SecureSocketOptions.StartTlsWhenAvailable);
-            _smtpClient.AuthenticateAsync(_options.Login, _options.Password);
-        }
+//        public void Connect()
+//        {
+//            _smtpClient.Connect(_options.Host, _options.Port, MailKit.Security.SecureSocketOptions.StartTlsWhenAvailable);
+//            _smtpClient.AuthenticateAsync(_options.Login, _options.Password);
+//        }
 
-        public async Task SendEmailWithHtmlAttachmentAsync(string email, string title, string content)
-        {
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("Pocket to Kindle", "pockettokindle@gmail.com"));
-            message.To.Add(new MailboxAddress(email));
-            message.Subject = title;
-            message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-            {
-                Text = content
-            };
+//        public async Task SendEmailWithHtmlAttachmentAsync(string email, string title, string content)
+//        {
+//            var message = new MimeMessage();
+//            message.From.Add(new MailboxAddress("Pocket to Kindle", "pockettokindle@gmail.com"));
+//            message.To.Add(new MailboxAddress(email));
+//            message.Subject = title;
+//            message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+//            {
+//                Text = content
+//            };
 
-            try
-            {
-                Connect();
-                await _smtpClient.SendAsync(message);
-                Console.WriteLine($"Successfully sent email: {message.Subject}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Sending message failed: {ex.Message}");
-            }
-            finally
-            {
-                await _smtpClient.DisconnectAsync(true);
-            }
-        }
-    }
-}
+//            try
+//            {
+//                Connect();
+//                await _smtpClient.SendAsync(message);
+//                Console.WriteLine($"Successfully sent email: {message.Subject}");
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine($"Sending message failed: {ex.Message}");
+//            }
+//            finally
+//            {
+//                await _smtpClient.DisconnectAsync(true);
+//            }
+//        }
+//    }
+//}

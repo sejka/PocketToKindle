cda1652da6d11aff5c57c172fa4a82cb82de49e4
sejka <karol.sejka@gmail.com>
2018-06-10 22:07:44 +0200

    upgrades cli projects to netcore2.1, adds mocking for emailsender testing

--- EmailSenderCLI/Program.cs
+++ EmailSenderCLI/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Core;
 using Core.EmailSenders;
+using Moq;
+using PocketSharp;
+using PocketSharp.Models;
 using PocketToKindle.Parsers;
 using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EmailSenderCLI
@@ -10,11 +15,33 @@ namespace EmailSenderCLI
     {
         private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             var config = new ConfigBuilder(".").Build();
-            var content = await new MercuryParser(config.MercuryApiKey, config.ServiceDomain, config.FunctionKey).ParseAsync("https://blog.wikimedia.org/2018/04/20/why-it-took-a-long-time-to-build-that-tiny-link-preview-on-wikipedia/");
-            var mailgunSender = new MailgunSender(config.MailGunSenderOptions.ApiKey, config.MailGunSenderOptions.HostEmail);
-            await mailgunSender.SendEmailWithHtmlAttachmentAsync("teherty_31b450@kindle.com", content.Title, $"<html><head><title>{content.Title}</title></head><body>{content.Content}</body></html>");
+            IParser parser = new MercuryParser(config.MercuryApiKey, config.ServiceDomain, config.FunctionKey);
+            var emailSender = new MailgunSender(config.MailGunSenderOptions.ApiKey, config.MailGunSenderOptions.HostEmail);
+            var pocketClientMock = new Mock<IPocketClient>();
+
+            pocketClientMock.Setup(x =>
+                x.Get(
+                    It.IsAny<State>(),
+                    It.IsAny<bool>(),
+                    It.IsAny<string>(),
+                    It.IsAny<ContentType>(),
+                    It.IsAny<Sort>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<DateTime>(),
+                    It.IsAny<int>(),
+                    It.IsAny<int>(),
+                    It.IsAny<CancellationToken>())).Returns(
+                    Task.FromResult(
+                        (IEnumerable<PocketItem>)new PocketItem[] {
+                            new PocketItem { Uri = new Uri("https://blog.wikimedia.org/2018/04/20/why-it-took-a-long-time-to-build-that-tiny-link-preview-on-wikipedia/") }
+                        }
+                    )
+                );
+
+            var sender = new Sender(pocketClientMock.Object, parser, emailSender);
+            await sender.SendAsync(new User { KindleEmail = "teherty@gmail.com" });
         }
     }
 }
